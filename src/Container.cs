b2a538b81db576b3ae72a1bc5bfef4a84c432b9d
Dan Mitreanu <dan@mitreanu.ro>
2023-01-06 15:15:15 +0200

    Request non-required dependencies

--- src/Container.cs
+++ src/Container.cs
@@ -36,7 +36,13 @@ public class Container
            throw new NetInjectException($"Cannot resolve dependency {iType.Name}. It is not registered.");
 
         var dep = _registered[iType];
-        dep.InstantiationInfo ??= GetInstantiationInfo(dep.Class);
+        if (dep.InstantiationInfo is null)
+        {
+            int validCtors = GetInstantiationInfo(dep.Class, out var instInfo);
+            dep.InstantiationInfo = instInfo ?? throw new NetInjectException(
+                $"Dependency {iType.Name} requires exactly 1 valid constructor (found {validCtors})."
+            );
+        }
 
         List<object> instances = new();
 
@@ -82,6 +88,81 @@ public class Container
         return dep.InstantiationInfo.Constructor.Invoke(instances.ToArray());
     }
 
+    private object? Request(Type iType, IEnumerable<Type> types, Dictionary<Type, object> scoped)
+    {
+        if (types.Contains(iType))
+            return null;
+
+        types = types.Append(iType);
+
+        if (!_registered.ContainsKey(iType))
+            return null;
+
+        var dep = _registered[iType];
+        if (dep.InstantiationInfo is null)
+        {
+            int validCtors = GetInstantiationInfo(dep.Class, out var instInfo);
+            if (validCtors != 1 || instInfo is null)
+                return null;
+
+            dep.InstantiationInfo = instInfo;
+        }
+
+        List<object> instances = new();
+
+        foreach (var depType in dep.InstantiationInfo.Dependencies)
+        {
+            var subdep = _registered[depType];
+            if (dep.Lifetime == Life.Singleton && subdep.Lifetime != Life.Singleton)
+                return null;
+
+            switch (subdep.Lifetime)
+            {
+                case Life.Singleton:
+                {
+                    if (!_singletons.ContainsKey(subdep.Interface))
+                    {
+                        var req = Request(subdep.Interface, types, scoped);
+                        if (req is null)
+                            return null;
+
+                        _singletons.Add(subdep.Interface, req);
+                    }
+
+                    instances.Add(_singletons[subdep.Interface]);
+                    break;
+                }
+
+                case Life.Scoped:
+                {
+                    if (!scoped.ContainsKey(subdep.Interface))
+                    {
+                        var req = Request(subdep.Interface, types, scoped);
+                        if (req is null)
+                            return null;
+
+                        scoped.Add(subdep.Interface, req);
+                    }
+
+                    instances.Add(scoped[subdep.Interface]);
+                    break;
+                }
+
+                case Life.Transient:
+                {
+                    var req = Request(subdep.Interface, types, scoped);
+                    if (req is null)
+                        return null;
+
+                    instances.Add(req);
+                    break;
+                }
+            }
+        }
+
+        return dep.InstantiationInfo.Constructor.Invoke(instances.ToArray());
+    }
+
     private void Add<I, T>(Life lifetime) where T : I
     {
         var i = typeof(I);
@@ -98,18 +179,19 @@ public class Container
         return !ctor.GetParameters().Any(p => !_registeredTypes.Contains(p.ParameterType));
     }
 
-    private InstantiationInfo GetInstantiationInfo(Type type)
+    private int GetInstantiationInfo(Type type, out InstantiationInfo? info)
     {
         var ctors = type.GetConstructors().Where(ctor => IsValidConstructor(ctor));
         int validCtors = ctors.Count();
 
-        if (validCtors == 0)
-            throw new NetInjectException($"Cannot find any valid constructor for dependency {type.Name}.");
-
         if (validCtors != 1)
-            throw new NetInjectException($"Found too many valid constructors for dependency {type.Name}.");
+        {
+            info = null;
+            return validCtors;
+        }
 
         var ctor = ctors.Single();
-        return new(ctor, ctor.GetParameters().Select(p => p.ParameterType));
+        info = new(ctor, ctor.GetParameters().Select(p => p.ParameterType));
+        return validCtors;
     }
 }
